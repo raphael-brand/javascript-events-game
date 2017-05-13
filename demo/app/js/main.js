fe045d0695bbcab2eda1236b38b94533e90f9dd4
Raphael Brand <info@raphaelbrand.de>
2017-05-13 23:35:32 +0200

    adding dragdrop callbacks

--- demo/app/js/main.js
+++ demo/app/js/main.js
@@ -1,6 +1,7 @@
 var rectDrags = false;
 var target = null;
 function dragRect(event) {
+  event.dataTransfer.setData("text", event.target.id);
   rectDrags = true;
   target = event.target;
   require(['test-module'], function(test) {
@@ -8,18 +9,29 @@ function dragRect(event) {
   });
 }
 function stopDragRect(event) {
+
+  event.preventDefault();
+  console.log(window.pageX);
+  document.querySelector('.two').style.left = event.target.clientX + 'px';
+  //document.querySelector('.one').style.top = target.style.top + 'px';
+  event.target.style.top;
+
   rectDrags = false;
 }
 
-function moveRect() {
-  if(rectDrags)
+function moveRect(x,y) {
+
+  if(rectDrags) {
     target.style.backgroundColor = '#fa0';
+    target.style.left = x + 'px';
+    target.style.top = y + 'px';
+  }
   else if(target){
     target.style.backgroundColor = 'transparent';
     target = null;
   }
 }
 
-window.addEventListener('mousemove', function() {
-    moveRect();
+window.addEventListener('mousemove', function(event) {
+    moveRect(event.clientX, event.clientY);
 });
