f4bd4f06322753d7740912cd46130906ae1a4f39
Raphael Brand <info@raphaelbrand.de>
2017-05-13 22:47:22 +0200

    Adding grid box and draggables

--- demo/app/js/main.js
+++ demo/app/js/main.js
@@ -1,3 +1,25 @@
-require(['test-module'], function(test) {
-  console.log(test.test());
+var rectDrags = false;
+var target = null;
+function dragRect(event) {
+  rectDrags = true;
+  target = event.target;
+  require(['test-module'], function(test) {
+    console.log(test.test());
+  });
+}
+function stopDragRect(event) {
+  rectDrags = false;
+}
+
+function moveRect() {
+  if(rectDrags)
+    target.style.backgroundColor = '#fa0';
+  else if(target){
+    target.style.backgroundColor = 'transparent';
+    target = null;
+  }
+}
+
+window.addEventListener('mousemove', function() {
+    moveRect();
 });
