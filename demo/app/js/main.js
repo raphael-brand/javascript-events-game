fdb983565d16d9310cb27b4bf3608626b8be5523
Raphael Brand <info@raphaelbrand.de>
2017-05-14 00:06:43 +0200

    adding mousover highlight

--- demo/app/js/main.js
+++ demo/app/js/main.js
@@ -1,7 +1,12 @@
 var rectDrags = false;
 var target = null;
+function hl_color(event) {
+  event.target.classList.add('highlight');
+}
+function rm_color(event) {
+  event.target.classList.remove('highlight');
+}
 function dragRect(event) {
-  event.dataTransfer.setData("text", event.target.id);
   rectDrags = true;
   target = event.target;
   require(['test-module'], function(test) {
@@ -9,29 +14,18 @@ function dragRect(event) {
   });
 }
 function stopDragRect(event) {
-
-  event.preventDefault();
-  console.log(window.pageX);
-  document.querySelector('.two').style.left = event.target.clientX + 'px';
-  //document.querySelector('.one').style.top = target.style.top + 'px';
-  event.target.style.top;
-
   rectDrags = false;
 }
 
-function moveRect(x,y) {
-
-  if(rectDrags) {
+function moveRect() {
+  if(rectDrags)
     target.style.backgroundColor = '#fa0';
-    target.style.left = x + 'px';
-    target.style.top = y + 'px';
-  }
   else if(target){
     target.style.backgroundColor = 'transparent';
     target = null;
   }
 }
 
-window.addEventListener('mousemove', function(event) {
-    moveRect(event.clientX, event.clientY);
+window.addEventListener('mousemove', function() {
+    moveRect();
 });
