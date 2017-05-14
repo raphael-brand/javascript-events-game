cd2be2f832e024250afc7d4a9d7533f9d4796f77
Raphael Brand <info@raphaelbrand.de>
2017-05-14 16:56:01 +0200

    module Rectangle added to test-module

--- demo/app/js/rectangle.js
+++ demo/app/js/rectangle.js
@@ -1,22 +1,29 @@
-var Rectangle = function (width, height, x, y) {
+define('rectangle', function() {
 
-  !x && (x = 0);
-  !y && (y = 0);
+  var Rectangle = function (width, height, x, y) {
 
-  var points = {
-    "a": [x, y],
-    "b": [x + width, y],
-    "c": [x, y + height],
-    "d": [x + width, y + height]
+    !x && (x = 0);
+    !y && (y = 0);
+
+    var points = {
+      "a": [x, y],
+      "b": [x + width, y],
+      "c": [x, y + height],
+      "d": [x + width, y + height]
+    }
+
+    this.points = points;
+    this.a = points.a;
+    this.b = points.b;
+    this.c = points.c;
+    this.d = points.d;
+    
+    this.getRect = function () {
+      return this.points;
+    }
   }
 
-  this.points = points;
-  this.a = points.a;
-  this.b = points.b;
-  this.c = points.c;
-  this.d = points.d;
-  
-  this.getRect = function () {
-    return this.points;
+  return {
+    Rectangle: Rectangle    
   }
-}
+});
