dd64c171b6ea941c3c3722e89f89c57e7123f80b
Raphael Brand <info@raphaelbrand.de>
2017-05-14 09:36:05 +0200

    merging from 755cddb88

--- demo/app/js/main.js
+++ demo/app/js/main.js
@@ -1,3 +1,31 @@
-require(['test-module'], function(test) {
-  console.log(test.test());
+var rectDrags = false;
+var target = null;
+function hl_color(event) {
+  event.target.classList.add('highlight');
+}
+function rm_color(event) {
+  event.target.classList.remove('highlight');
+}
+function dragRect(event) {
+  rectDrags = true;
+  target = event.target;
+  require(['test-module'], function(test) {
+    console.log(test.test());
+  });
+}
+function stopDragRect(event) {
+  rectDrags = false;
+}
+
+function moveRect() {
+  if(rectDrags)
+    target.style.backgroundColor = '#fa0';
+  else if(target){
+    target.style.backgroundColor = 'transparent';
+    target = null;
+  }
+}
+
+window.addEventListener('mousemove', function() {
+    moveRect();
 });
