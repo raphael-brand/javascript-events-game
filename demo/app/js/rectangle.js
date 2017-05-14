00b8ffd2e9b9d980f5f8353acc123ea071bf2891
Raphael Brand <info@raphaelbrand.de>
2017-05-14 16:42:32 +0200

    gulp tasks js-vendor: added js: rectangle added

---
+++ demo/app/js/rectangle.js
@@ -0,0 +1,22 @@
+var Rectangle = function (width, height, x, y) {
+
+  !x && (x = 0);
+  !y && (y = 0);
+
+  var points = {
+    "a": [x, y],
+    "b": [x + width, y],
+    "c": [x, y + height],
+    "d": [x + width, y + height]
+  }
+
+  this.points = points;
+  this.a = points.a;
+  this.b = points.b;
+  this.c = points.c;
+  this.d = points.d;
+  
+  this.getRect = function () {
+    return this.points;
+  }
+}
